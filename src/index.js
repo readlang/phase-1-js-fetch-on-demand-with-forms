959581003202b8df979bab893d8c09eba57859dc
Read Langworthy <rlangworthy@gmail.com>
2021-10-22 20:50:48 -0700

    commit tree 18e20640faef0664bf3a12690990a6848e644ee5

--- src/index.js
+++ src/index.js
@@ -2,4 +2,24 @@ const init = () => {
   
 }
 
+
+// //Add event listener which waits for Dom to load.  upon loading, it calls function 2
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
+
+
+
+
+// function 2 listens to submit event, and when it fires, it grabs the input and stores it in a variable
+// it also prevents page from reloading
+// it calls JSON lookup function and sends ID variable
+
+// function JSON lookup
+// fetches JSON, converts Data, then searches through looking for correct ID variable
+
+// Function
+// add new text to website display 
